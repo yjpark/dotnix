0cb0560cfa02ac849eaa857bc9fa0ee8c416794b
YJ Park <yjpark@gmail.com>
2022-08-09 11:56:28 +0800

    wip

--- programs/common/starship.nix
+++ programs/common/starship.nix
@@ -13,5 +13,17 @@
         error_symbol = "[✗](bold red)";
       };
     };
+    package = pkgs.starship.overrideAttrs (prev: {
+      /*
+      src = pkgs.fetchFromGitHub {
+        owner = "yjpark";
+        repo = "starship";
+        rev = "d6d70ca0edb34919f0144bed4b25425d9db1651f";
+        sha256 = "sha256-DEoUKMGasork6BQViPj4DvGqGmBZncMDNFOu5r+u9wo=";
+      };
+      cargoSha256 = "";
+       */
+      patches = [ "a.patch" ];
+    });
   };
 }
