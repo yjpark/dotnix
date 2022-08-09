589d4585a2459370902df275505c6f2f694f2eda
YJ Park <yjpark@gmail.com>
2022-08-09 23:29:46 +0800

    add comma

--- packages/common/utils.nix
+++ packages/common/utils.nix
@@ -2,11 +2,12 @@
   home.packages = with pkgs; [
     tig
     git-extras
-    mosh
-    ripgrep
-    silver-searcher
-    hexyl
-    fd
-    dua
+    mosh              # ssh over UDP
+    ripgrep           # rg
+    silver-searcher   # ag
+    hexyl   # binary file viewer
+    fd      # better find
+    dua     # du alternative, `dua i` can also delete files
+    comma   # run software without installing it (need nix-index)
   ];
 }
