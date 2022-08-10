c67d89329855c49ef61caf58e1ee6a492cc868fd
YJ Park <yjpark@gmail.com>
2022-08-10 17:05:53 +0000

    setup gpg-agent

--- programs/common/gpg.nix
+++ programs/common/gpg.nix
@@ -2,4 +2,11 @@
   programs.gpg = {
     enable = true;
   };
+  services.gpg-agent = {
+    enable = true;
+    pinentryFlavor = "curses";
+  };
+  home.packages = with pkgs; [
+    pinentry.curses
+  ];
 }
