fc9b2fc3b9fad2de640c3d905c3946898d6af503
YJ Park <yjpark@gmail.com>
2022-08-10 16:25:38 +0800

    add gpg

--- programs/common/default.nix
+++ programs/common/default.nix
@@ -10,6 +10,7 @@
     ./git.nix
     ./fzf.nix
     ./tmux.nix
+    ./gpg.nix
     ./password-store.nix
     ./nushell.nix
     ./alacritty.nix
---
+++ programs/common/gpg.nix
@@ -0,0 +1,5 @@
+{ pkgs, ... }: {
+  programs.gpg = {
+    enable = true;
+  };
+}
