55323d7830a5104cc9f2e52bff1bc4346528d28a
YJ Park <yjpark@gmail.com>
2022-08-11 16:13:13 +0800

    allow unfree for wsl

--- hosts/wsl/default.nix
+++ hosts/wsl/default.nix
@@ -4,6 +4,7 @@
     ../../nixos/settings/system.nix
     ../../nixos/settings/nix-flakes.nix
     ../../nixos/settings/nix-mirror.nix
+    ../../nixos/settings/nix-unfree.nix
     ../../nixos/services/openssh.nix
     ../../nixos/pkgs
     ../../nixos/users
