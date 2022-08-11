9f7abd243910e078e065c290698d43e0a4d8e7aa
YJ Park <yjpark@gmail.com>
2022-08-11 15:47:35 +0000

    tweak proxy setter

--- programs/common/fish/aliases.nix
+++ programs/common/fish/aliases.nix
@@ -8,7 +8,13 @@
     set-proxy-panda = "set -gx https_proxy 127.0.0.1:1104; set -gx http_proxy 127.0.0.1:1104; set -gx ALL_PROXY http://127.0.0.1:1104; set -gx NIX_CURL_FLAGS http://127.0.0.1:1104";
     set-proxy-cow = "set -gx https_proxy 127.0.0.1:1100; set -gx http_proxy 127.0.0.1:1100; set -gx ALL_PROXY http://127.0.0.1:1100; set -gx NIX_CURL_FLAGS http://127.0.0.1:1100";
     set-proxy-mitm = "set -gx https_proxy 127.0.0.1:1111; set -gx http_proxy 127.0.0.1:1111; set -gx ALL_PROXY http://127.0.0.1:1111";
-    set-proxy-lan = "set -gx https_proxy 10.0.1.6:1102; set -gx http_proxy 10.0.1.6:1102; set -gx ALL_PROXY http://10.0.1.6:1102; set -gx NIX_CURL_FLAGS http://10.0.1.6:1102";
-    set-proxy-desktop = "set -gx https_proxy 10.0.1.9:1102; set -gx http_proxy 10.0.1.9:1102; set -gx ALL_PROXY http://10.0.1.9:1102; set -gx NIX_CURL_FLAGS http://10.0.1.9:1102";
+    set-proxy-2 = "set -gx https_proxy 10.0.1.2:1102; set -gx http_proxy 10.0.1.2:1102; set -gx ALL_PROXY http://10.0.1.2:1102; set -gx NIX_CURL_FLAGS http://10.0.1.2:1102";
+    set-proxy-3 = "set -gx https_proxy 10.0.1.3:1102; set -gx http_proxy 10.0.1.3:1102; set -gx ALL_PROXY http://10.0.1.3:1102; set -gx NIX_CURL_FLAGS http://10.0.1.3:1102";
+    set-proxy-4 = "set -gx https_proxy 10.0.1.4:1102; set -gx http_proxy 10.0.1.4:1102; set -gx ALL_PROXY http://10.0.1.4:1102; set -gx NIX_CURL_FLAGS http://10.0.1.4:1102";
+    set-proxy-5 = "set -gx https_proxy 10.0.1.5:1102; set -gx http_proxy 10.0.1.5:1102; set -gx ALL_PROXY http://10.0.1.5:1102; set -gx NIX_CURL_FLAGS http://10.0.1.5:1102";
+    set-proxy-6 = "set -gx https_proxy 10.0.1.6:1102; set -gx http_proxy 10.0.1.6:1102; set -gx ALL_PROXY http://10.0.1.6:1102; set -gx NIX_CURL_FLAGS http://10.0.1.6:1102";
+    set-proxy-7 = "set -gx https_proxy 10.0.1.7:1102; set -gx http_proxy 10.0.1.7:1102; set -gx ALL_PROXY http://10.0.1.7:1102; set -gx NIX_CURL_FLAGS http://10.0.1.7:1102";
+    set-proxy-8 = "set -gx https_proxy 10.0.1.8:1102; set -gx http_proxy 10.0.1.8:1102; set -gx ALL_PROXY http://10.0.1.8:1102; set -gx NIX_CURL_FLAGS http://10.0.1.8:1102";
+    set-proxy-9 = "set -gx https_proxy 10.0.1.9:1102; set -gx http_proxy 10.0.1.9:1102; set -gx ALL_PROXY http://10.0.1.9:1102; set -gx NIX_CURL_FLAGS http://10.0.1.9:1102";
   };
 }
