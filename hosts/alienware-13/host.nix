8abdc4d4c437ad4e0c8ddc276073bf90510b3a4d
YJ Park <yjpark@gmail.com>
2022-08-11 17:38:41 +0000

    wakeOnLan setting been changed

--- hosts/alienware-13/host.nix
+++ hosts/alienware-13/host.nix
@@ -3,10 +3,5 @@
 {
   networking.hostId = "80b98880";
   networking.hostName = "alienware-13";
-  services.wakeonlan.interfaces = [
-    {
-      interface = "enp2s0";
-      method = "magicpacket";
-    }
-  ];
+  networking.interfaces.enp2s0.wakeOnLan.enable = true;
 }
--- hosts/edger/host.nix
+++ hosts/edger/host.nix
@@ -3,10 +3,5 @@
 {
   networking.hostId = "f4769ef1";
   networking.hostName = "edger";
-  services.wakeonlan.interfaces = [
-    {
-      interface = "enp37s0";
-      method = "magicpacket";
-    }
-  ];
+  networking.interfaces.enp37s0.wakeOnLan.enable = true;
 }
