fe8dd66bb801ddc6cf1f10ad0180abc438f1cbef
YJ Park <yjpark@gmail.com>
2022-08-12 22:05:56 +0800

    setup path for private folders

--- programs/common/fish/init/path.fish
+++ programs/common/fish/init/path.fish
@@ -2,7 +2,16 @@ if test -d ~/.nix/bin/common
     set -x PATH $PATH ~/.nix/bin/common
 end
 
+if test -d ~/.nix.private/bin/common
+    set -x PATH $PATH ~/.nix.private/bin/common
+end
+
 uname -v | grep NixOS > /dev/null
 if [ $status -eq 0 ]
-    set -x PATH $PATH ~/.nix/bin/nixos
+    if test -d ~/.nix/bin/nixos
+        set -x PATH $PATH ~/.nix/bin/nixos
+    end
+    if test -d ~/.nix.private/bin/nixos
+        set -x PATH $PATH ~/.nix.private/bin/nixos
+    end
 end
