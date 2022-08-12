288a24ac2be829a2bd645d1485dcc6a3d1e539e0
YJ Park <yjpark@gmail.com>
2022-08-12 18:41:04 +0800

    disable wayland for rustdesk to work

--- nixos/services/normal/xserver.nix
+++ nixos/services/normal/xserver.nix
@@ -9,5 +9,9 @@
   services.xserver.layout = "us";
   services.xserver.xkbVariant = "dvorak";
   services.xserver.exportConfiguration = true;
+
+  # rustdesk doesn't support wayland ATM
+  # https://github.com/rustdesk/rustdesk/issues/56
+  services.xserver.displayManager.gdm.wayland = false;
 }
 
