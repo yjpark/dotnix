cf91bf6f4d679a2fc40088ad824c92d9b52c7a87
YJ Park <yjpark@gmail.com>
2022-08-19 18:43:40 +0800

    tweak k3s config

--- nixos/services/dev/k3s.nix
+++ nixos/services/dev/k3s.nix
@@ -2,6 +2,9 @@
   services.k3s = {
     enable = true;
     role = "server";
+    extraFlags = toString [
+      "--no-deploy metrics-server"
+    ];
   };
   networking.firewall.allowedTCPPorts = [
     6443
