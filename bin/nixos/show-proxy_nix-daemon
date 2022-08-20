e5b33da4e44f24ea5885ee395a2809f0405beece
YJ Park <yjpark@gmail.com>
2022-08-20 15:24:54 +0800

    setup k3s with zfs, and hacking proxy setting for it

---
+++ bin/nixos/show-proxy_nix-daemon
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+sudo systemctl cat nix-daemon | grep _proxy
+sudo systemctl cat nix-daemon | grep NIX_CURL_FLAGS
--- nixos/services/dev/k3s.nix
+++ nixos/services/dev/k3s.nix
@@ -3,9 +3,30 @@
     enable = true;
     role = "server";
     extraFlags = toString [
-      "--no-deploy metrics-server"
+      "--container-runtime-endpoint unix:///run/containerd/containerd.sock"
+      # "--no-deploy metrics-server"
     ];
   };
+  # Note: need to create the zfs mount manually
+  # sudo zfs create -o mountpoint=/var/lib/containerd/io.containerd.snapshotter.v1.zfs rpool/state/containerd
+  virtualisation.containerd = {
+    enable = true;
+    settings =
+      let
+        fullCNIPlugins = pkgs.buildEnv {
+          name = "full-cni";
+          paths = with pkgs;[
+            cni-plugins
+            cni-plugin-flannel
+          ];
+        };
+      in {
+        plugins."io.containerd.grpc.v1.cri".cni = {
+          bin_dir = "${fullCNIPlugins}/bin";
+          conf_dir = "/var/lib/rancher/k3s/agent/etc/cni/net.d/";
+        };
+      };
+  }; 
   networking.firewall.allowedTCPPorts = [
     6443
   ];
