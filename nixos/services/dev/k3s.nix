130826f0d65e86b91bc992bfe315b487c134b9fb
YJ Park <yjpark@gmail.com>
2022-08-24 00:21:44 +0800

    cleanup

--- nixos/services/dev/k3s.nix
+++ nixos/services/dev/k3s.nix
@@ -4,7 +4,6 @@
     role = "server";
     extraFlags = toString [
       "--container-runtime-endpoint unix:///run/containerd/containerd.sock"
-      "--no-deploy metrics-server"
     ];
   };
   # Note: need to create the zfs mount manually
