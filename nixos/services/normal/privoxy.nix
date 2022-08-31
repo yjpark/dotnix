e7141d3676d71c4c52d23eed04690604f70b9d3a
YJ Park <yjpark@gmail.com>
2022-08-31 12:11:33 +0800

    setup privoxy forward actions

--- nixos/services/normal/privoxy.nix
+++ nixos/services/normal/privoxy.nix
@@ -3,9 +3,15 @@
     enable = true;
     settings = {
         listen-address = "0.0.0.0:1102";
-        forward-socks5 = "/ localhost:1101 .";
+        # forward-socks5 = "/ localhost:1101 .";
     };
   };
+  environment.etc = {
+    "privoxy/forward.action".source = ./privoxy.forward.action;
+  };
+  services.privoxy.settings.actionsFiles = [
+    "/etc/privoxy/forward.action"
+  ];
   networking.firewall.allowedTCPPorts = [
     1101
     1102
