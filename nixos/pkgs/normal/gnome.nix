bc8b690733e1c843b849e9e44b502867ff2108f1
YJ Park <yjpark@gmail.com>
2022-08-31 16:24:27 +0800

    add unite

--- nixos/pkgs/normal/gnome.nix
+++ nixos/pkgs/normal/gnome.nix
@@ -1,6 +1,7 @@
 { config, pkgs, ... }: {
   environment.systemPackages = with pkgs; [
     gnome.gnome-tweaks
+    gnomeExtensions.unite
     gnomeExtensions.appindicator
     gnomeExtensions.clipboard-indicator
     gnomeExtensions.system-monitor-next
