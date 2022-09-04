3adfb6836cb3ff5e388eca56a9164c4177295cbb
YJ Park <yjpark@gmail.com>
2022-09-04 22:28:37 +0800

    hide Unity title bar

--- programs/linux/devilspie2.nix
+++ programs/linux/devilspie2.nix
@@ -5,6 +5,9 @@
       if (get_window_class() == "org.wezfurlong.wezterm") then
         undecorate_window();
       end
+      if (get_window_class() == "Unity") then
+        undecorate_window();
+      end
     '';
   };
 }
