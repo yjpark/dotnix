c53ac27e9cd1dd6311d69e9e8d4a45923f324da4
YJ Park <yjpark@gmail.com>
2022-09-05 15:22:02 +0800

    cleanup mirrors setting

--- nixos/settings/common/nix-mirror.nix
+++ nixos/settings/common/nix-mirror.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 {
   nix.settings.substituters = pkgs.lib.mkBefore [
-    "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
+    "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store/"
   ];
 }
 
