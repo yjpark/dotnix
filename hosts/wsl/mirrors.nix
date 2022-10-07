ae07324b6ca92eced6bc7233f9a81a65944dcf63
YJ Park <yjpark@gmail.com>
2022-10-07 18:32:07 +0800

    not using edger mirror for now

--- hosts/wsl/mirrors.nix
+++ hosts/wsl/mirrors.nix
@@ -1,8 +1,8 @@
 { config, pkgs, ... }:
 {
-  nix.settings.substituters = pkgs.lib.mkBefore [
-    "http://edger.yjpark.zerotier:1050/"
-  ];
+  # nix.settings.substituters = pkgs.lib.mkBefore [
+  #   "http://edger.yjpark.zerotier:1050/"
+  # ];
   nix.settings.trusted-public-keys = [
     "edger:TCLFJAq2OeCXAZ5kr548+cuW3k4VpBVHjpslrARzpeg="
   ];
