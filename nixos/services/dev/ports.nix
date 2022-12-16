8c03f7e4c0636fd2c5d047aeb386d2cd2f70a6a8
YJ Park <yjpark@gmail.com>
2022-12-16 15:39:58 +0800

    tweak ports

--- nixos/services/dev/ports.nix
+++ nixos/services/dev/ports.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }: {
   networking.firewall.allowedTCPPortRanges = [
-    3000
-    8080
+    { from = 3000; to = 3010; }
+    { from = 8080; to = 8090; }
     { from = 29000; to = 29999; }
   ];
 }
