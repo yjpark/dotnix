1e9ae6654462875666b6356027c21b1d8f26ca8e
YJ Park <yjpark@gmail.com>
2023-03-06 14:31:34 +0800

    open more dev ports

--- nixos/services/dev/ports.nix
+++ nixos/services/dev/ports.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }: {
   networking.firewall.allowedTCPPortRanges = [
-    { from = 3000; to = 3010; }
-    { from = 8080; to = 8090; }
+    { from = 3000; to = 3999; }
+    { from = 8000; to = 8999; }
     { from = 29000; to = 29999; }
   ];
 }
