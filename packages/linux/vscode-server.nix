3bd80c30463b34045a687a424ef52e727b926f03
YJ Park <yjpark@gmail.com>
2023-03-23 15:58:32 +0800

    update vscode-server

--- packages/linux/vscode-server.nix
+++ packages/linux/vscode-server.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }: {
   imports = [
     "${fetchTarball {
-        url = "https://github.com/msteen/nixos-vscode-server/tarball/867d5691871acc039fb47d45018c69e9ac751d95";
+        url = "https://github.com/msteen/nixos-vscode-server/tarball/57f1716bc625d2892579294cc207956679e3d94c";
         sha256 = "sha256:1dr3v3mlf61nrs3f3d9qx74y8v5jihkk8wd1li4sglx22aqh4xf6";
        }
      }/modules/vscode-server/home.nix"
