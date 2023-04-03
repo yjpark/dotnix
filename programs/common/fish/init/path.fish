650db264095314edab06dd2e2e544469e8f99cd8
YJ Park <yjpark@gmail.com>
2023-04-03 17:50:13 +0800

    tweak fish PATH

--- programs/common/fish/init/path.fish
+++ programs/common/fish/init/path.fish
@@ -1,39 +1,39 @@
 if test -d ~/.nix/bin/common
-    set -x PATH $PATH ~/.nix/bin/common
+    set -x PATH ~/.nix/bin/common $PATH
 end
 
 if test -d ~/.nix.private/bin/common
-    set -x PATH $PATH ~/.nix.private/bin/common
+    set -x PATH ~/.nix.private/bin/common $PATH
 end
 
 uname -v | grep NixOS > /dev/null
 if [ $status -eq 0 ]
     if test -d ~/.nix/bin/nixos
-        set -x PATH $PATH ~/.nix/bin/nixos
+        set -x PATH ~/.nix/bin/nixos $PATH
     end
     if test -d ~/.nix.private/bin/nixos
-        set -x PATH $PATH ~/.nix.private/bin/nixos
+        set -x PATH ~/.nix.private/bin/nixos $PATH
     end
 end
 
 uname -v | grep Ubuntu > /dev/null
 if [ $status -eq 0 ]
-    set -x PATH $PATH ~/.nix-profile/bin
+    set -x PATH ~/.nix-profile/bin $PATH
     if test -d ~/.nix/bin/ubuntu
-        set -x PATH $PATH ~/.nix/bin/ubuntu
+        set -x PATH ~/.nix/bin/ubuntu $PATH
     end
     if test -d ~/.nix.private/bin/ubuntu
-        set -x PATH $PATH ~/.nix.private/bin/ubuntu
+        set -x PATH ~/.nix.private/bin/ubuntu $PATH
     end
 end
 
 uname | grep Linux > /dev/null
 if [ $status -eq 0 ]
     if test -d ~/.nix/bin/linux
-        set -x PATH $PATH ~/.nix/bin/linux
+        set -x PATH ~/.nix/bin/linux $PATH
     end
     if test -d ~/.nix.private/bin/linux
-        set -x PATH $PATH ~/.nix.private/bin/linux
+        set -x PATH ~/.nix.private/bin/linux $PATH
     end
 end
 
