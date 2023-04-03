eab6c3874aad9c10a9c33b4ed6b75cdbd7137352
YJ Park <yjpark@gmail.com>
2023-04-03 16:57:58 +0800

    update hash

--- packages/linux/vscode-server.nix
+++ packages/linux/vscode-server.nix
@@ -2,7 +2,7 @@
   imports = [
     "${fetchTarball {
         url = "https://github.com/msteen/nixos-vscode-server/tarball/57f1716bc625d2892579294cc207956679e3d94c";
-        sha256 = "sha256:1dr3v3mlf61nrs3f3d9qx74y8v5jihkk8wd1li4sglx22aqh4xf6";
+        sha256 = "sha256:0ahgyd2swkapimvf70ah2y55wpn2hdh1wymfh6492xrkv5x91sqz";
        }
      }/modules/vscode-server/home.nix"
   ];
