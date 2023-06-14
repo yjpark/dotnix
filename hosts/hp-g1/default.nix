2ea984be382d97e7adde0735e21b67c830b86613
YJ Park <yjpark@gmail.com>
2023-06-14 19:39:27 +0800

    enable k3s for g1

--- hosts/hp-g1/default.nix
+++ hosts/hp-g1/default.nix
@@ -7,6 +7,7 @@
     ../../nixos/boot
     ../../nixos/settings
     ../../nixos/services/normal
+    ../../nixos/services/dev
     ../../nixos/pkgs
     ../../nixos/users
     ../../nixos/services/normal/nosleep.nix
