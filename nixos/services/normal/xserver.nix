e1327f9126829b5907d766ffc040b1b8ee160396
YJ Park <yjpark@gmail.com>
2023-06-17 23:48:44 +0800

    switch to wayland

--- nixos/services/normal/xserver.nix
+++ nixos/services/normal/xserver.nix
@@ -9,9 +9,5 @@
   services.xserver.layout = "us";
   services.xserver.xkbVariant = "dvorak";
   services.xserver.exportConfiguration = true;
-
-  # rustdesk doesn't support wayland ATM
-  # https://github.com/rustdesk/rustdesk/issues/56
-  services.xserver.displayManager.gdm.wayland = false;
 }
 
