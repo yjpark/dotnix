2db81af9b986f8f2ca091097b37dfa537861fd2f
YJ Park <yjpark@gmail.com>
2023-07-07 13:12:22 +0800

    add lapce settings

--- packages/common/trail.nix
+++ packages/common/trail.nix
@@ -4,4 +4,6 @@
     jql                 # JSON tool
     lapce               # editor
   ];
+  home.file.".config/lapce-nightly/settings.toml".source = ./lapce/settings.toml;
+  home.file.".config/lapce-nightly/keymaps.toml".source = ./lapce/keymaps.toml;
 }
