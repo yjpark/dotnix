d6b9fc52731aecc29103001f5e67bc7b49e58f99
YJ Park <yjpark@gmail.com>
2023-07-16 23:30:01 +0800

    update versions

--- hosts/wsl/default.nix
+++ hosts/wsl/default.nix
@@ -11,7 +11,7 @@
     enable = true;
     # https://github.com/nix-community/NixOS-WSL/issues/179
     # need to disable nativeSystemd when building the installer
-    nativeSystemd = true;
+    # nativeSystemd = true;
     defaultUser = "yjpark";
     startMenuLaunchers = true;
     # use podman instead
