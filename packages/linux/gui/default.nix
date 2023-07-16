b6e17545484ef74513d92cda5dd5b0397e3bdab3
YJ Park <yjpark@gmail.com>
2023-07-16 16:17:13 +0800

    add corectrl

--- packages/linux/gui/default.nix
+++ packages/linux/gui/default.nix
@@ -13,6 +13,7 @@
     ydotool
     gnome-randr
     gjs # JavaScript bindings for GNOME
+    corectrl
   ];
   home.file.".config/run-or-raise/shortcuts.conf".source = ./run-or-raise/shortcuts.conf;
 }
