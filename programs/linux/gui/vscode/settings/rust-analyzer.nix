b12a8c602c959ab426ce1e0503799cdd6f049b86
YJ Park <yjpark@gmail.com>
2023-07-25 13:12:21 +0800

    small tweak with abbrs

--- programs/linux/gui/vscode/settings/rust-analyzer.nix
+++ programs/linux/gui/vscode/settings/rust-analyzer.nix
@@ -1,6 +1,9 @@
 { config, pkgs, ... }: {
     programs.vscode.userSettings = {
-        "rust-analyzer.checkOnSave" = false;
+      # "rust-analyzer.checkOnSave" = false;
+      "rust-analyzer.files.excludeDirs" = [
+        "**/external"
+      ];
     };
 }
 
