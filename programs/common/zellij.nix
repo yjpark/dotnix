1a04174ccd5fd19b47ee71035f97f8d2e6892315
YJ Park <yjpark@gmail.com>
2024-05-11 17:16:33 +0800

    update zellij config

--- programs/common/zellij.nix
+++ programs/common/zellij.nix
@@ -1,36 +1,14 @@
 { pkgs, ... }: {
   programs.zellij = {
     enable = true;
-    settings = {
-      default_layout = "compact";
-      theme = "molokai-dark";
-      pane_frames = false;
-      keybindings = ''
-        normal clear-defaults=true {
-            bind "Ctrl g" { SwitchToMode "locked"; }
-            bind "Ctrl b" { SwitchToMode "tmux"; }
-        }
-        locked {
-            bind "Ctrl g" { SwitchToMode "Normal"; }
-        }
-        tmux {
-              bind "1" { GoToTab 1; SwitchToMode "Normal"; }
-              bind "2" { GoToTab 2; SwitchToMode "Normal"; }
-              bind "3" { GoToTab 3; SwitchToMode "Normal"; }
-              bind "4" { GoToTab 4; SwitchToMode "Normal"; }
-              bind "5" { GoToTab 5; SwitchToMode "Normal"; }
-              bind "6" { GoToTab 6; SwitchToMode "Normal"; }
-              bind "7" { GoToTab 7; SwitchToMode "Normal"; }
-              bind "8" { GoToTab 8; SwitchToMode "Normal"; }
-              bind "9" { GoToTab 9; SwitchToMode "Normal"; }
-              bind "e" { EditScrollback; SwitchToMode "Normal"; }
-              bind "m" { SwitchToMode "move"; }
-              bind "=" { SwitchToMode "resize"; }
-        }
-        shared_except "locked" {
-              bind "Ctrl g" { SwitchToMode "Locked"; }
-        }
-      '';
-    };
+    #settings = {
+    #  theme = "molokai-dark";
+    #  pane_frames = false;
+    #};
   };
+  # https://github.com/nix-community/home-manager/pull/4465
+  # it's quite tricky to config mappings with home-manager
+  home.file.".config/zellij/config.kdl".source = ./zellij/config.kdl;
+  home.file.".config/zellij/layouts/default.kdl".source = ./zellij/layouts/default.kdl;
+  home.file.".config/zellij/plugins/zjstatus.wasm".source = ./zellij/plugins/zjstatus.wasm;
 }
