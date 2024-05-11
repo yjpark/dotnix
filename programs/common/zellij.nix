4a1214b682309e2f8223c3abc406eb71c5fe29a1
YJ Park <yjpark@gmail.com>
2024-05-11 16:03:27 +0800

    update zellij bindings

--- programs/common/zellij.nix
+++ programs/common/zellij.nix
@@ -4,6 +4,32 @@
     settings = {
       default_layout = "compact";
       theme = "molokai-dark";
+      keybindings = ''
+        normal clear-defaults=true {
+            bind "Ctrl g" { SwitchToMode "locked"; }
+            bind "Ctrl b" { SwitchToMode "tmux"; }
+        }
+        locked {
+            bind "Ctrl g" { SwitchToMode "Normal"; }
+        }
+        tmux {
+              bind "1" { GoToTab 1; SwitchToMode "Normal"; }
+              bind "2" { GoToTab 2; SwitchToMode "Normal"; }
+              bind "3" { GoToTab 3; SwitchToMode "Normal"; }
+              bind "4" { GoToTab 4; SwitchToMode "Normal"; }
+              bind "5" { GoToTab 5; SwitchToMode "Normal"; }
+              bind "6" { GoToTab 6; SwitchToMode "Normal"; }
+              bind "7" { GoToTab 7; SwitchToMode "Normal"; }
+              bind "8" { GoToTab 8; SwitchToMode "Normal"; }
+              bind "9" { GoToTab 9; SwitchToMode "Normal"; }
+              bind "e" { EditScrollback; SwitchToMode "Normal"; }
+              bind "m" { SwitchToMode "move"; }
+              bind "=" { SwitchToMode "resize"; }
+        }
+        shared_except "locked" {
+              bind "Ctrl g" { SwitchToMode "Locked"; }
+        }
+      '';
     };
   };
 }
