f38ee109130c9ca2b3813813e3df932d9b5097c0
YJ Park <yjpark@gmail.com>
2024-11-14 13:38:50 +0800

    updates after upgrading

--- hosts/alienware-13/samba.nix
+++ hosts/alienware-13/samba.nix
@@ -10,20 +10,20 @@
     enable = true;
     openFirewall = true;
     securityType = "user";
-    extraConfig = ''
-      workgroup = WORKGROUP
-      server string = alienware-13
-      netbios name = alienware-13
-      security = user
-      #use sendfile = yes
-      #max protocol = smb2
-      # note: localhost is the ipv6 localhost ::1
-      hosts allow = 10.0.1. 127.0.0.1 localhost
-      hosts deny = 0.0.0.0/0
-      guest account = nobody
-      map to guest = bad user
-    '';
-    shares = {
+    settings = {
+      global = {
+        workgroup = "WORKGROUP";
+        "server string" = "alienware-13";
+        "netbios name" = "alienware-13";
+        security = "user";
+        #use sendfile = yes
+        #max protocol = smb2
+        # note: localhost is the ipv6 localhost ::1
+        "hosts allow" = "10.0.1. 127.0.0.1 localhost";
+        "hosts deny" = "0.0.0.0/0";
+        "guest account" = "nobody";
+        "map to guest" = "bad user";
+      };
       public = {
         path = "/data/public";
         browseable = "yes";
--- programs/linux/gui/vscode/extensions/dev.nix
+++ programs/linux/gui/vscode/extensions/dev.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }: {
   programs.vscode.extensions = with pkgs.vscode-extensions; [
     jnoortheen.nix-ide
-    matklad.rust-analyzer
+    rust-lang.rust-analyzer
     zxh404.vscode-proto3
     ionide.ionide-fsharp
     ms-dotnettools.csharp
