9448a6d7aad892e54c9a9ddaa05a1a7ddfa780b9
YJ Park <yjpark@gmail.com>
2025-04-22 23:48:15 +0800

    update proxy

--- programs/common/fish/aliases.nix
+++ programs/common/fish/aliases.nix
@@ -6,14 +6,12 @@
     reset-proxy = "set -ge https_proxy; set -ge http_proxy; set -ge all_proxy; set -ge NIX_CURL_FLAGS";
     show-proxy = "env | grep _proxy; env | grep NIX_CURL_FLAGS";
     set-proxy = "set -gx https_proxy http://proxy:31102; set -gx http_proxy http://proxy:31102; set -gx all_proxy http://proxy:31102; set -gx NIX_CURL_FLAGS -x http://proxy:31102";
-    set-proxy-trojan = "set -gx https_proxy http://127.0.0.1:1102; set -gx http_proxy http://127.0.0.1:1102; set -gx all_proxy http://127.0.0.1:1102; set -gx NIX_CURL_FLAGS -x http://127.0.0.1:1102";
-    set-proxy-panda = "set -gx https_proxy http://127.0.0.1:1104; set -gx http_proxy http://127.0.0.1:1104; set -gx all_proxy http://127.0.0.1:1104; set -gx NIX_CURL_FLAGS -x http://127.0.0.1:1104";
-    set-proxy-cow = "set -gx https_proxy http://127.0.0.1:1100; set -gx http_proxy http://127.0.0.1:1100; set -gx all_proxy http://127.0.0.1:1100; set -gx NIX_CURL_FLAGS -x http://127.0.0.1:1100";
+    set-proxy-trojan = "set -gx https_proxy http://127.0.0.1:31102; set -gx http_proxy http://127.0.0.1:31102; set -gx all_proxy http://127.0.0.1:31102; set -gx NIX_CURL_FLAGS -x http://127.0.0.1:31102";
     set-proxy-mitm = "set -gx https_proxy http://127.0.0.1:1111; set -gx http_proxy http://127.0.0.1:1111; set -gx all_proxy http://127.0.0.1:1111";
     set-proxy-edger = "set -gx https_proxy http://172.22.1.2:31102; set -gx http_proxy http://172.22.1.2:31102; set -gx all_proxy http://172.22.1.2:31102; set -gx NIX_CURL_FLAGS -x http://172.22.1.2:31102";
     set-proxy-p2 = "set -gx https_proxy http://172.22.1.5:31102; set -gx http_proxy http://172.22.1.5:31102; set -gx all_proxy http://172.22.1.5:31102; set -gx NIX_CURL_FLAGS -x http://172.22.1.5:31102";
     set-proxy-a13 = "set -gx https_proxy http://172.22.1.6:31102; set -gx http_proxy http://172.22.1.6:31102; set -gx all_proxy http://172.22.1.6:31102; set -gx NIX_CURL_FLAGS -x http://172.22.1.6:31102";
-    set-proxy-pc = "set -gx https_proxy http://172.22.1.9:1102; set -gx http_proxy http://172.22.1.9:1102; set -gx all_proxy http://172.22.1.9:1102; set -gx NIX_CURL_FLAGS -x http://172.22.1.9:1102";
+    set-proxy-pc = "set -gx https_proxy http://172.22.1.9:31102; set -gx http_proxy http://172.22.1.9:31102; set -gx all_proxy http://172.22.1.9:31102; set -gx NIX_CURL_FLAGS -x http://172.22.1.9:31102";
     set-proxy-edger_lan = "set -gx https_proxy http://10.0.1.2:31102; set -gx http_proxy http://10.0.1.2:31102; set -gx all_proxy http://10.0.1.2:31102; set -gx NIX_CURL_FLAGS -x http://10.0.1.2:31102";
     set-proxy-p2_lan = "set -gx https_proxy http://10.0.1.5:31102; set -gx http_proxy http://10.0.1.5:31102; set -gx all_proxy http://10.0.1.5:31102; set -gx NIX_CURL_FLAGS -x http://10.0.1.5:31102";
     set-proxy-a13_lan = "set -gx https_proxy http://10.0.1.6:31102; set -gx http_proxy http://10.0.1.6:31102; set -gx all_proxy http://10.0.1.6:31102; set -gx NIX_CURL_FLAGS -x http://10.0.1.6:31102";
